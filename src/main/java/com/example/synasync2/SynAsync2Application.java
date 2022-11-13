af89092e9c5e71885494eedf9643903b0182e40b
Robert Witt Hansen <along_the_black_river@hotmail.com>
2022-11-13 14:51:20 +0100

    Initial commit

---
+++ src/main/java/com/example/synasync2/SynAsync2Application.java
@@ -0,0 +1,13 @@
+package com.example.synasync2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynAsync2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynAsync2Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/synasync2/apiController/NameController.java
@@ -0,0 +1,2 @@
+package com.example.synasync2.apiController;public class NameController {
+}
